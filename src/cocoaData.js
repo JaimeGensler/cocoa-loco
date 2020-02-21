a94df8f1f665e41ffc636dbe5302724d97cffd9f
Jaime Gensler <jaimegensler0@gmail.com>
2020-02-21 14:03:42 -0800

    Restructure data

--- src/cocoaData.js
+++ src/cocoaData.js
@@ -1,132 +1,124 @@
-const cocoaData = {
-    white: [
-        {
-            itemName: 'Peppermint White Chocolate Cocoa',
-            imgURL:
-                'https://www.gimmesomeoven.com/wp-content/uploads/2014/12/Peppermint-White-Hot-Chocolate-2.jpg',
-            description:
-                'Light, minty, and delicious peppermint cocoa topped with real peppermint chunks.',
-            price: 3.25,
-            cocoaContent: 25,
-        },
-        {
-            itemName: 'Eggnog Cocoa',
-            imgURL:
-                'https://www.inkatrinaskitchen.com/wp-content/uploads/2016/11/Eggnog-Hot-Chocolate-10-wm-500.jpg?m',
-            description: 'Eggnog-infused drink with white chocolate shavings.',
-            price: 3.5,
-            cocoaContent: 25,
-        },
-        {
-            itemName: 'Alien Cocoa',
-            imgURL:
-                'https://www.easypeasyandfun.com/wp-content/uploads/2015/09/Halloween-Hot-Chocolate.jpg',
-            description:
-                'This cocoa’s out of this world- complete with whip, sprinkles, and candy eyes. Your friends will be green with envy!',
-            price: 3.75,
-            cocoaContent: 0,
-        },
-    ],
-    milk: [
-        {
-            itemName: 'Standard House Cocoa',
-            imgURL:
-                'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fcdn-image.travelandleisure.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F1600x1000%2Fpublic%2F1544042819%2Ffour-seasons-vail-colorado-HOTCOCO1218.jpg%3Fitok%3D2dEnNODU&w=400&c=sc&poi=face&q=85',
-            description:
-                'Our delicious house cocoa, made with milk chocolate and topped with a marshmallow.',
-            price: 3.0,
-            cocoaContent: 40,
-        },
-        {
-            itemName: 'Gingerbread Cocoa',
-            imgURL:
-                'https://www.cravingsofalunatic.com/wp-content/uploads/2017/12/Gingerbread-Hot-Chocolate-7.jpg',
-            description:
-                'Smooth gingerbread-flavored cocoa topped with mini marshmallows and a whole gingerbread friend.',
-            price: 3.75,
-            cocoaContent: 50,
-        },
-        {
-            itemName: 'Gold Digger’s Cocoa',
-            imgURL:
-                'https://media.ksdk.com/assets/KSDK/images/8c7a4c8d-857c-4a2e-9611-8ccd93ef5a53/8c7a4c8d-857c-4a2e-9611-8ccd93ef5a53_360x203.jpg',
-            description: 'Milk chocolate cocoa with real gold leaf.',
-            price: 55.0,
-            cocoaContent: 40,
-        },
-        {
-            itemName: 'Frozen Cocoa',
-            imgURL:
-                'https://www.melskitchencafe.com/wp-content/uploads/frozen-hot-chocolate3-720x540.jpg',
-            description:
-                'For days when it’s just too warm for regular cocoa, try our blended frozen cocoa.',
-            price: 4.25,
-            cocoaContent: 60,
-        },
-        {
-            itemName: 'Pumpkin Spice Hot Cocoa',
-            imgURL:
-                'https://i2.wp.com/sugarhero.com/wp-content/uploads/2017/08/pumpkin-spice-hot-chocolate-truffles-7.jpg',
-            description:
-                'Cozy cocoa with pumpkin spice topped with whip and fall sprinkles.',
-            price: 3.5,
-            cocoaContent: 60,
-        },
-        {
-            itemName: 'Salted Peanut Butter Cocoa',
-            imgURL:
-                'https://www.pumpkinnspice.com/wp-content/uploads/2015/11/peanut-butter-cup-hot-chocolate4-200x300.jpg',
-            description:
-                'Deliciously rich peanut butter cocoa topped with sea salt, peanut butter cups, and real peanut butter.',
-            price: 6.2,
-            cocoaContent: 62,
-        },
-    ],
-    dark: [
-        {
-            itemName: 'Mexican Cocoa',
-            imgURL:
-                'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fcdn-image.realsimple.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmarquee_large_2x%2Fpublic%2F1481212808%2Fmexican-hot-cocoa.jpg%3Fitok%3DBk1lwZVb',
-            description:
-                'Just like abuelita used to make: warm, rich, cocoa with a kick of cinnamon and cayenne.',
-            price: 3.25,
-            cocoaContent: 75,
-        },
-        {
-            itemName: 'Midnight Cocoa',
-            imgURL:
-                'https://www.mysequinedlife.com/MSLblog/wp-content/uploads/2017/10/thick-and-creamy-mocha-hot-chocolate-8a.jpg',
-            description:
-                'Our darkest cocoa in a rich, whole milk. Reminiscent of a moonless night.',
-            price: 3.0,
-            cocoaContent: 86,
-        },
-        {
-            itemName: 'Espresso Cocoa',
-            imgURL:
-                'https://assets.bonappetit.com/photos/57accdae1b334044149751d8/16:9/w_1200,c_limit/Hot-Chocolate-3-of-5.jpg',
-            description:
-                'Hot cocoa made with espresso. Topped with whipped cream, chocolate shavings, and chocolate-covered espresso beans.',
-            price: 4.0,
-            cocoaContent: 80,
-        },
-        {
-            itemName: 'Jaime’s Cocoa',
-            imgURL:
-                'https://xebiijq7c0-flywheel.netdna-ssl.com/wp-content/uploads/2013/12/spicy-cinnamon-hot-chocolate-6-2.jpg',
-            description: 'House blend of cocoa, maple, cinnamon, and love.',
-            price: 3.5,
-            cocoaContent: 75,
-        },
-        {
-            itemName: 'Valentine’s Cocoa',
-            imgURL:
-                'https://cdn.cupcakeproject.com/wp-content/uploads/2016/02/Sugar-Pie-Hot-Chocolate-01.jpg',
-            description: 'Cocoa for you to share with that special someone.',
-            price: 7.25,
-            cocoaContent: 70,
-        },
-    ],
-};
-
-export default cocoaData;
+export const cocoaData = [
+    {
+        itemName: 'Peppermint White Chocolate Cocoa',
+        imgURL:
+            'https://www.gimmesomeoven.com/wp-content/uploads/2014/12/Peppermint-White-Hot-Chocolate-2.jpg',
+        description:
+            'Light, minty, and delicious peppermint cocoa topped with real peppermint chunks.',
+        price: 3.25,
+        cocoaContent: 25,
+    },
+    {
+        itemName: 'Eggnog Cocoa',
+        imgURL:
+            'https://www.inkatrinaskitchen.com/wp-content/uploads/2016/11/Eggnog-Hot-Chocolate-10-wm-500.jpg?m',
+        description: 'Eggnog-infused drink with white chocolate shavings.',
+        price: 3.5,
+        cocoaContent: 25,
+    },
+    {
+        itemName: 'Alien Cocoa',
+        imgURL:
+            'https://www.easypeasyandfun.com/wp-content/uploads/2015/09/Halloween-Hot-Chocolate.jpg',
+        description:
+            'This cocoa’s out of this world- complete with whip, sprinkles, and candy eyes. Your friends will be green with envy!',
+        price: 3.75,
+        cocoaContent: 0,
+    },
+    {
+        itemName: 'Standard House Cocoa',
+        imgURL:
+            'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fcdn-image.travelandleisure.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F1600x1000%2Fpublic%2F1544042819%2Ffour-seasons-vail-colorado-HOTCOCO1218.jpg%3Fitok%3D2dEnNODU&w=400&c=sc&poi=face&q=85',
+        description:
+            'Our delicious house cocoa, made with milk chocolate and topped with a marshmallow.',
+        price: 3.0,
+        cocoaContent: 40,
+    },
+    {
+        itemName: 'Gingerbread Cocoa',
+        imgURL:
+            'https://www.cravingsofalunatic.com/wp-content/uploads/2017/12/Gingerbread-Hot-Chocolate-7.jpg',
+        description:
+            'Smooth gingerbread-flavored cocoa topped with mini marshmallows and a whole gingerbread friend.',
+        price: 3.75,
+        cocoaContent: 50,
+    },
+    {
+        itemName: 'Gold Digger’s Cocoa',
+        imgURL:
+            'https://media.ksdk.com/assets/KSDK/images/8c7a4c8d-857c-4a2e-9611-8ccd93ef5a53/8c7a4c8d-857c-4a2e-9611-8ccd93ef5a53_360x203.jpg',
+        description: 'Milk chocolate cocoa with real gold leaf.',
+        price: 55.0,
+        cocoaContent: 40,
+    },
+    {
+        itemName: 'Frozen Cocoa',
+        imgURL:
+            'https://www.melskitchencafe.com/wp-content/uploads/frozen-hot-chocolate3-720x540.jpg',
+        description:
+            'For days when it’s just too warm for regular cocoa, try our blended frozen cocoa.',
+        price: 4.25,
+        cocoaContent: 60,
+    },
+    {
+        itemName: 'Pumpkin Spice Hot Cocoa',
+        imgURL:
+            'https://i2.wp.com/sugarhero.com/wp-content/uploads/2017/08/pumpkin-spice-hot-chocolate-truffles-7.jpg',
+        description:
+            'Cozy cocoa with pumpkin spice topped with whip and fall sprinkles.',
+        price: 3.5,
+        cocoaContent: 60,
+    },
+    {
+        itemName: 'Salted Peanut Butter Cocoa',
+        imgURL:
+            'https://www.pumpkinnspice.com/wp-content/uploads/2015/11/peanut-butter-cup-hot-chocolate4-200x300.jpg',
+        description:
+            'Deliciously rich peanut butter cocoa topped with sea salt, peanut butter cups, and real peanut butter.',
+        price: 6.2,
+        cocoaContent: 62,
+    },
+    {
+        itemName: 'Mexican Cocoa',
+        imgURL:
+            'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fcdn-image.realsimple.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmarquee_large_2x%2Fpublic%2F1481212808%2Fmexican-hot-cocoa.jpg%3Fitok%3DBk1lwZVb',
+        description:
+            'Just like abuelita used to make: warm, rich, cocoa with a kick of cinnamon and cayenne.',
+        price: 3.25,
+        cocoaContent: 75,
+    },
+    {
+        itemName: 'Midnight Cocoa',
+        imgURL:
+            'https://www.mysequinedlife.com/MSLblog/wp-content/uploads/2017/10/thick-and-creamy-mocha-hot-chocolate-8a.jpg',
+        description:
+            'Our darkest cocoa in a rich, whole milk. Reminiscent of a moonless night.',
+        price: 3.0,
+        cocoaContent: 86,
+    },
+    {
+        itemName: 'Espresso Cocoa',
+        imgURL:
+            'https://assets.bonappetit.com/photos/57accdae1b334044149751d8/16:9/w_1200,c_limit/Hot-Chocolate-3-of-5.jpg',
+        description:
+            'Hot cocoa made with espresso. Topped with whipped cream, chocolate shavings, and chocolate-covered espresso beans.',
+        price: 4.0,
+        cocoaContent: 80,
+    },
+    {
+        itemName: 'Jaime’s Cocoa',
+        imgURL:
+            'https://xebiijq7c0-flywheel.netdna-ssl.com/wp-content/uploads/2013/12/spicy-cinnamon-hot-chocolate-6-2.jpg',
+        description: 'House blend of cocoa, maple, cinnamon, and love.',
+        price: 3.5,
+        cocoaContent: 75,
+    },
+    {
+        itemName: 'Valentine’s Cocoa',
+        imgURL:
+            'https://cdn.cupcakeproject.com/wp-content/uploads/2016/02/Sugar-Pie-Hot-Chocolate-01.jpg',
+        description: 'Cocoa for you to share with that special someone.',
+        price: 7.25,
+        cocoaContent: 70,
+    },
+];
